4eb9c5dee2cd7a3f4093cf7b4a6e85eff064adf9
TouchSeyha <hcy.contactbs@gmail.com>
2025-12-27 12:03:19 +0700

    fix: update Google sign-in email verification to check for '@gmail.com'

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -60,7 +60,7 @@ export const authConfig = {
   callbacks: {
     async signIn({ account, profile }) {
       if (account?.provider === 'google') {
-        return !!(profile?.email_verified && profile?.email?.endsWith('@example.com'));
+        return !!(profile?.email_verified && profile?.email?.endsWith('@gmail.com'));
       }
       return true; // Do different verification for other providers that don't have `email_verified`
     },
