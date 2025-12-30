1fc5ea09acc11a94a943617c61cd57c0c2c0a03e
TouchSeyha <hcy.contactbs@gmail.com>
2025-12-30 16:06:45 +0700

    feat: add @vercel/speed-insights integration to layout and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '~/styles/globals.css';
 import { type Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { TRPCReactProvider } from '~/trpc/react';
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className={`${geist.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
